3890b4e4a69c9a34587beb36766f124111fcfa20
Carlos <capz2003@gmail.com>
2022-01-15 15:59:29 -0500

    Creates errors

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,8 @@ const connectDb = require('./db/connectDb')
 const transactions = require('./router/transactions')
 
 const app = express()
-
 const port = process.env.port || 3001
-
+app.use(express.json())
 
 //routes
 app.use("/transactions", transactions);
--- backend/controllers/transactions.js
+++ backend/controllers/transactions.js
@@ -1,17 +1,40 @@
+const Transaction = require('../models/transaction')
 
-const getAllTransaction = (req,res, next)=>{
-    res.send('get all transactions')
+
+const getAllTransaction = async(req,res, next)=>{
+    try {
+        const transaction = await Transaction.find({});
+        res.status(200).json({ count: transaction.length, data: transaction });  
+        
+    } catch (error) {
+        res.status(500).json({msg:'server error'}) 
+    }
+    
 }
 
-const getTransaction = (req, res, next) => {
+const getTransaction = async (req, res, next) => {
   res.send("delete a transaction");
 };
 
-const createTransaction = (req, res, next) => {
-  res.send("create a trasaction");
+const createTransaction = async (req, res, next) => {
+    const {text,amount} = req.body
+    try {
+         const transaction = await Transaction.create(req.body);
+         res.status(201).json({ data: transaction });
+        
+    } catch (error) {
+        if(error.name === 'ValidationError'){
+            const message = Object.values(error.errors).map(val=>val.message)
+            return res.status(400).json({error:message})
+        }
+        else {
+            return res.status(500).json({error: 'server error'})
+        }
+    }
+   
 };
 
-const deleteTransaction = (req, res, next) => {
+const deleteTransaction = async (req, res, next) => {
   res.send("delete a transaction");
 };
 
