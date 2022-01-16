7dc72c7ed023c94c2601d7903939f11449871f89
Carlos <capz2003@gmail.com>
2022-01-16 08:41:22 -0500

    Creates globalContext

---
+++ frontend/src/Context/GlobalState.js
@@ -0,0 +1,21 @@
+import React, {createContext, useReducer} from ' react'
+import AppReducer from './AppReducer'
+
+//initial state
+const InitialState = {
+  transactions: [
+   { id: 1, text: " Flowers", amount: -28 },
+   { id: 2, text: " Salary", amount: 300 },
+   { id: 3, text: " Book", amount: -10 },
+   { id: 4, text: " Camera", amount: 150 }
+]};
+
+//createContext
+
+export const GlobalContext = createContext(InitialState);
+
+//provider
+export const GlobalProvider = ({children}) =>{
+    const [state, dispatch] = useReducer(AppReducer, InitialState);
+    return (<GlobalContext.Provider value={{transactions:state.transactions}}>{children}</GlobalContext.Provider>)
+}
--- frontend/src/components/AddTransaction.js
+++ frontend/src/components/AddTransaction.js
@@ -5,22 +5,35 @@ const [text, setText]= useState('')
 const [amount, setAmount]=useState(0)
 
     return (
-        <>
+      <>
         <h3>Add new Transaction</h3>
         <form action="">
-            <div className="form-control">
-                <label htmlFor="text">Text</label>
-                <input type="text" placeholder='Enter text...' />
-            </div>
-            <div className="form-control">
-                <label htmlFor="amount">Amount<br/>(negative-expense, positie -cincome)</label>
-                <input type="number" placeholder='Enter Amount...' />
-            </div>
-            <button className="btn">Add Transaction</button>
+          <div className="form-control">
+            <label htmlFor="text">Text</label>
+            <input
+              type="text"
+              placeholder="Enter text..."
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">
+              Amount
+              <br />
+              (negative-expense, positie -cincome)
+            </label>
+            <input
+              type="number"
+              placeholder="Enter Amount..."
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+          </div>
+          <button className="btn">Add Transaction</button>
         </form>
-            
-        </>
-    )
+      </>
+    );
 }
 
 export default AddTransaction;
