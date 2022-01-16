c7f363de9e02be487e2642899a6f87de35a4c5a1
Carlos <capz2003@gmail.com>
2022-01-16 09:07:47 -0500

    Fixes balance component

--- frontend/src/components/Balance.js
+++ frontend/src/components/Balance.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useContext} from "react";
+import { GlobalContext } from "../Context/GlobalState";
 
 const Balance = () => {
-  return <div>Balance</div>;
+const {transactions} = useContext(GlobalContext)
+
+  return (
+    <>
+      <h4>Balance</h4>
+      <h1>0.00</h1>
+    </>
+  ); 
 };
 
 export default Balance;
