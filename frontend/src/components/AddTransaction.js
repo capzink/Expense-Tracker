883d223ef155bb00fb9a8d51f833c6340bc2400c
Carlos <capz2003@gmail.com>
2022-01-16 10:03:25 -0500

    Adds add Transaction

--- frontend/src/components/AddTransaction.js
+++ frontend/src/components/AddTransaction.js
@@ -1,14 +1,29 @@
-import React,{useState} from 'react'
+import React,{useState,useContext} from 'react'
 import { GlobalContext } from '../Context/GlobalState'
 
 const AddTransaction = () => {
+ const { addTransaction } = useContext(GlobalContext);
 const [text, setText]= useState('')
 const [amount, setAmount]=useState(0)
+
+const onSubmit = e =>{
+e.preventDefault()
+const newTransaction = {
+    id: Math.floor(Math.random()*100000),
+    text,
+    amount:+amount
+    
+}
+
+
+addTransaction(newTransaction);
+
+}
 
     return (
       <>
         <h3>Add new Transaction</h3>
-        <form action="">
+        <form onSubmit={onSubmit}>
           <div className="form-control">
             <label htmlFor="text">Text</label>
             <input
