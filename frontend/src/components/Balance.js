3e9990ec231cb86e80ca1d2628fbb396f3143163
Carlos <capz2003@gmail.com>
2022-01-16 09:15:34 -0500

    Sets match to cinome expense

--- frontend/src/components/Balance.js
+++ frontend/src/components/Balance.js
@@ -3,11 +3,13 @@ import { GlobalContext } from "../Context/GlobalState";
 
 const Balance = () => {
 const {transactions} = useContext(GlobalContext)
+const amount = transactions.map(transaction=>transaction.amount)
+const total = amount.reduce((acc,item)=>acc+=item,0).toFixed(2)
 
   return (
     <>
       <h4>Balance</h4>
-      <h1>0.00</h1>
+      <h1>{total}</h1>
     </>
   ); 
 };
