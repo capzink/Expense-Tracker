4048df5f3327179b92130623065ce687ab4fcc75
Carlos <capz2003@gmail.com>
2022-01-17 18:08:11 -0500

    Adds create and delete from database functionality

--- frontend/src/Context/GlobalState.js
+++ frontend/src/Context/GlobalState.js
@@ -34,11 +34,36 @@ async function getTransactions(){
     
   }
 }
-function deleteTransaction(id){
+async function deleteTransaction(id){
+  try {
+    await axios.delete((`/transactions/${id}`))
     dispatch({type: 'DELETE_TRANSACTION',payload:id})
+  } catch (error) {
+    dispatch({
+      type: "TRANSACTION_ERROR",
+      payload: error.response.data.error,
+    });
+    
+  }
 }
-function addTransaction(transaction) {
-  dispatch({ type: "ADD_TRANSACTION", payload: transaction });
+
+async function addTransaction(transaction) {
+  const config = {
+    header:{
+      'Content-type':'application/json'
+    }
+  }
+  try {
+    const res= await axios.post('/transactions',transaction,config)
+    dispatch({ type: "ADD_TRANSACTION", payload: res.data.data });
+  } catch (error) {
+    dispatch({
+      type: "TRANSACTION_ERROR",
+      payload: error.response.data.error,
+    });
+    
+    
+  }
 }
 
     return (
